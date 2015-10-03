ada959052fa3a3df80818795b8ee5e0bc494d853
jaturon <iceoton@gmail.com>
2015-10-04 01:29:56 +0700

    Replace and add new menu picture in assets folder.

--- app/src/main/java/com/itonlab/kitcher/MainActivity.java
+++ app/src/main/java/com/itonlab/kitcher/MainActivity.java
@@ -124,7 +124,7 @@ public class MainActivity extends Activity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.main, menu);
         //return false it will not be shown.
         return false;
     }
